e9012538bfe5ee23c16a246360118b8dd088ed4f
Javier Alexander Perez Arevalo <ja.pereza@uniandes.edu.co>
2020-03-14 22:40:32 -0500

    Screenshots (con Cypress) de la primera fase

---
+++ cypress/integration/simple_spec.js
@@ -0,0 +1,25 @@
+describe('Tres pruebas de generacion de paletas ', function () {
+
+    it('Visitar la pagina desarrollada en la primera fase', function () {
+        cy.visit('https://japereza.github.io/taller06-VRT-primerafase/palette.html')
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
+        cy.screenshot('CapturaInicial');
+    })
+
+    it('Prueba 1 con screenshots', function () {
+        cy.get('#nuevapaleta').click()
+        cy.screenshot('CapturaPrueba01');
+    })
+
+    it('Prueba 2 con screenshots', function () {
+        cy.get('#nuevapaleta').click()
+        cy.screenshot('CapturaPrueba02');
+    })
+
+    it('Prueba 3 con screenshots', function () {
+        cy.get('#nuevapaleta').click()
+        cy.screenshot('CapturaPrueba03');
+    })
+})
